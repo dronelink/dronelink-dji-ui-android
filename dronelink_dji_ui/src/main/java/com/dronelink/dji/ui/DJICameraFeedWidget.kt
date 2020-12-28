46f12842be3d8a343d663400e918243d22a572ec
Jim McAndrew <>
2020-12-28 14:55:23 -0600

    - Updated dependencies - Set source camera name visibility to false - Updated gradle files

--- dronelink_dji_ui/src/main/java/com/dronelink/dji/ui/DJICameraFeedWidget.kt
+++ dronelink_dji_ui/src/main/java/com/dronelink/dji/ui/DJICameraFeedWidget.kt
@@ -1,33 +1,29 @@
 package com.dronelink.dji.ui
 
 import android.os.Bundle
-import android.util.DisplayMetrics
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
-import androidx.fragment.app.Fragment
 import com.dronelink.core.ui.widget.Widget
-import dji.common.camera.SettingsDefinitions
 import dji.ux.widget.FPVWidget
 
 class DJICameraFeedWidget: Widget() {
-
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val frameLayout = FrameLayout(requireContext())
         frameLayout.layoutParams = FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT)
 
+        //FIXME does not reliably show the video feed when loaded like this?
         val fpvWidget = FPVWidget(requireContext())
         val layoutParams = FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT)
         layoutParams.gravity = Gravity.CENTER
         fpvWidget.layoutParams = layoutParams
         fpvWidget.videoSource = FPVWidget.VideoSource.PRIMARY
+        fpvWidget.setSourceCameraNameVisibility(false)
 
         frameLayout.addView(fpvWidget)
 
         return frameLayout
     }
-
 }
--- dronelink_dji_ui/src/main/java/com/dronelink/dji/ui/DronelinkDJIUI.kt
+++ dronelink_dji_ui/src/main/java/com/dronelink/dji/ui/DronelinkDJIUI.kt
@@ -7,26 +7,17 @@ import com.dronelink.core.ui.widget.WidgetFactory
 import com.dronelink.core.ui.widget.WidgetFactoryProvider
 import com.dronelink.dji.DJIDroneSessionManager
 
-class DronelinkDJIUI {
-
-}
-
 class DJIUIDroneSessionManager(val context: Context): DJIDroneSessionManager(context), WidgetFactoryProvider {
-
     override val widgetFactory: WidgetFactory?
         get() = DJIWidgetFactory(context, session)
-
 }
 
 open class DJIWidgetFactory(val context: Context, session: DroneSession?): WidgetFactory(session) {
-
     override fun createCameraFeedWidget(primary: Boolean): Widget? {
         if (session == null) {
             return null
         }
 
         return DJICameraFeedWidget()
-
     }
-
 }
