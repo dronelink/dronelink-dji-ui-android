ac6cc0c0688e0948e014d2a4cf00875f45680eee
matidr <matias@fueled.com>
2021-02-11 14:16:20 -0300

    Add camera settings widget and fix Video Feed

--- dronelink_dji_ui/src/main/java/com/dronelink/dji/ui/DJICameraFeedWidget.kt
+++ dronelink_dji_ui/src/main/java/com/dronelink/dji/ui/DJICameraFeedWidget.kt
@@ -1,29 +1,24 @@
 package com.dronelink.dji.ui
 
 import android.os.Bundle
-import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
 import com.dronelink.core.ui.widget.Widget
+import com.dronelink.dji.ui.R.layout
 import dji.ux.widget.FPVWidget
 
-class DJICameraFeedWidget: Widget() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val frameLayout = FrameLayout(requireContext())
-        frameLayout.layoutParams = FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT)
+class DJICameraFeedWidget : Widget() {
 
-        //FIXME does not reliably show the video feed when loaded like this?
-        val fpvWidget = FPVWidget(requireContext())
-        val layoutParams = FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT)
-        layoutParams.gravity = Gravity.CENTER
-        //FIXME fpvWidget.layoutParams = layoutParams
-        //FIXME fpvWidget.videoSource = FPVWidget.VideoSource.PRIMARY
-        //FIXME fpvWidget.setSourceCameraNameVisibility(false)
-
-        //FIXME frameLayout.addView(fpvWidget)
-
-        return frameLayout
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(layout.widget_camera_feed, container, false)
+        val fpvWidget = view.findViewById<FPVWidget>(R.id.feed)
+        fpvWidget.videoSource = FPVWidget.VideoSource.PRIMARY
+        fpvWidget.setSourceCameraNameVisibility(false)
+        return view
     }
 }
--- dronelink_dji_ui/src/main/java/com/dronelink/dji/ui/DronelinkDJIUI.kt
+++ dronelink_dji_ui/src/main/java/com/dronelink/dji/ui/DronelinkDJIUI.kt
@@ -2,22 +2,20 @@ package com.dronelink.dji.ui
 
 import android.content.Context
 import com.dronelink.core.DroneSession
+import com.dronelink.core.ui.widget.UpdatableWidget
 import com.dronelink.core.ui.widget.Widget
 import com.dronelink.core.ui.widget.WidgetFactory
 import com.dronelink.core.ui.widget.WidgetFactoryProvider
 import com.dronelink.dji.DJIDroneSessionManager
 
+
 class DJIUIDroneSessionManager(val context: Context): DJIDroneSessionManager(context), WidgetFactoryProvider {
-    override val widgetFactory: WidgetFactory?
+    override val widgetFactory: WidgetFactory
         get() = DJIWidgetFactory(context, session)
 }
 
 open class DJIWidgetFactory(val context: Context, session: DroneSession?): WidgetFactory(session) {
-    override fun createCameraFeedWidget(primary: Boolean): Widget? {
-        if (session == null) {
-            return null
-        }
-
+    override fun createCameraFeedWidget(primary: Boolean): Widget {
         return DJICameraFeedWidget()
     }
 }
